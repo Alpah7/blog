dd350c463d30116a7293e2e8132b163151a0b907
LinuxSec <yuyudhn@outlook.com>
2016-03-29 18:19:23 +0700

    Update adsantilok.js

--- js/adsantilok.js
+++ js/adsantilok.js
@@ -1,6 +1,7 @@
 /*
  * jQuery Anti Adblock
  * @copyright Hendriono http://modification-blog.blogspot.com
+ * Edited Didik Ariyana http://didikariyana16.blogspot.com
  * @version 1.00 (9 Maret 2011)
  */
 var standby = false;
@@ -34,8 +35,10 @@ document.write('<link rel="stylesheet" href="'+ linkcss +'" type="text/css">');
 
 function jAntiBlock() {
     jQuery(function () {
-        jQuery("body").append('<div id="energysaving"><p>Anda tidak suka iklan?<br /><span>Saya juga. <br/>Tapi anda harus tahu bahwa iklan sangat membantu blog ini agar tetap tersedia dan membuat saya termotivasi untuk terus menulis hal-hal yang menarik pada blog ini.<br/> Mohon pertimbangannya untuk menonaktifkan perangkat lunak blokir iklan (misal; Adblock) khusus untuk blog ini. <br/>Sebagai ucapan terima kasih, saya akan memberikan apresiasi yang setinggi-tingginya jika suatu hari kita bertemu dikehidupan nyata.<br/><br/>' + adminblog + '</span></p><div id="copyrightOnlineLeaf">Copyright 2013 <font style="color: #666666;">2012 - 2016</font> - All rights reserved></div></div>');
+        jQuery(document).ready(function () {
+            setTimeout("CekBlok();", 1000);
+            jQuery("body").append('<div id="energysaving"><p>Anda tidak suka iklan?<br /><span>Sama, Saya juga. <br/>Tapi anda harus tahu bahwa iklan sangat membantu blog ini agar tetap tersedia dan membuat saya termotivasi untuk terus menulis hal-hal yang menarik pada blog ini.<br/> Mohon pertimbangannya untuk menonaktifkan perangkat lunak blokir iklan (misal: Adblock) khusus untuk blog ini. <br/><i><b>Setelah Anda menonaktifkan AdBlock, silahkan Reload kembali Halaman ini dengan menekan F5.</b></i><br/> Dan saya meminta Maaf jika anda merasa Iklan diBlog ini Mengganggu anda<br/>Sebagai ucapan terima kasih, saya akan memberikan apresiasi yang setinggi-tingginya jika suatu hari kita bertemu dikehidupan nyata.<br/>' + adminblog + '</span></p><div id="copyrightOnlineLeaf">Copyright 2016 <font style="color: #666666;"> DH</font> - All rights reserved</div></div>');
         });
     });
 }
-jAntiBlock();
+jAntiBlock();// JavaScript Document
